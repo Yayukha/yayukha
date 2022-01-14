89758d025f905697a5af1f18335c18741c25933f
sergeyzel <zelionniysergey@yandex.ru>
2022-01-14 11:52:04 +0700

    zadanie

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Chapter2.Task9;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Chapter2.Task9.Program.run();
+        }
+    }
+}
